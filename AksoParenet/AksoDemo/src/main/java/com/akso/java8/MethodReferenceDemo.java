36fe7693254908d616b3124b1d63ba6ef9f0f070
leo365zhou <leo_365@126.com>
2019-06-27 18:06:38 +0800

    init

---
+++ AksoParenet/AksoDemo/src/main/java/com/akso/java8/MethodReferenceDemo.java
@@ -0,0 +1,13 @@
+package com.akso.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferenceDemo {
+
+    public static void main(String[] args) {
+        List<String> list  = Arrays.asList("hello", "world", "hello hi", "how are you");
+
+        list.forEach(System.out::println);
+    }
+}
