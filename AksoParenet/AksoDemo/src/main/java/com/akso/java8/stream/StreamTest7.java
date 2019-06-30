6639589c2550c7764d69fe1277ac917a236102be
git365zhou <leo_365@126.com>
2019-06-30 23:06:13 +0800

    init

---
+++ AksoParenet/AksoDemo/src/main/java/com/akso/java8/stream/StreamTest7.java
@@ -0,0 +1,17 @@
+package com.akso.java8.stream;
+
+import java.util.stream.Stream;
+
+public class StreamTest7 {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("hello", "world", "hello world" );
+//        stream.map(item -> item.substring(0, 1).toUpperCase() + item.substring(1)).forEach(System.out::println);
+
+        stream.map(item -> {
+            String res = item.substring(0, 1).toUpperCase() + item.substring(1);
+            System.out.println(res);
+            return res;
+        });
+    }
+
+}
