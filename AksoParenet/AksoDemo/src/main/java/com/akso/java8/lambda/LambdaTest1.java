9477d8e4a86ce8f97e29d953822e71f1fae78e3f
git365zhou <leo_365@126.com>
2019-07-01 22:43:25 +0800

    init

---
+++ AksoParenet/AksoDemo/src/main/java/com/akso/java8/lambda/LambdaTest1.java
@@ -0,0 +1,15 @@
+package com.akso.java8.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaTest1 {
+
+    public static void main(String[] args) {
+        String test = "test";
+        List<String> list = Arrays.asList("hello", "world", "hello world");
+        list.stream().map(x -> {
+            return x + " haha " + test;
+        }).forEach(System.out::println);
+    }
+}
