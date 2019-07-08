19c86191e434f91a4d71e3c84449da78e0902278
leo365zhou <leo_365@126.com>
2019-07-08 17:19:42 +0800

    init

---
+++ AksoParenet/AksoDemo/src/main/java/com/akso/java8/annotation/AnnotationTest.java
@@ -0,0 +1,29 @@
+package com.akso.java8.annotation;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * 重复注解和类型注解
+ */
+public class AnnotationTest {
+
+    @MyAnnotation("hello")
+    @MyAnnotation("world")
+    public void show(@MyAnnotation("abc") String str) {
+
+    }
+
+    @Test
+    public void test() throws NoSuchMethodException {
+        Class<AnnotationTest> clazz = AnnotationTest.class;
+        Method m1 = clazz.getMethod("show", String.class);
+
+        MyAnnotation[] myAnnotations =  m1.getAnnotationsByType(MyAnnotation.class);
+        System.out.println("-----------");
+        for (MyAnnotation annotation: myAnnotations) {
+            System.out.println(annotation.value());
+        }
+    }
+}
