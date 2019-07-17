81849f3cc456042dc8c691f394158f12073756f6
git365zhou <leo_365@126.com>
2019-07-17 22:26:36 +0800

    init

--- AksoParenet/AksoDemo/src/main/java/com/akso/jvm/CanReliveObj.java
+++ AksoParenet/AksoDemo/src/main/java/com/akso/jvm/CanReliveObj.java
@@ -2,6 +2,28 @@ package com.akso.jvm;
 
 public class CanReliveObj {
     public static CanReliveObj obj;
+    public static void main(String[] args) throws InterruptedException {
+        obj = new CanReliveObj();
+        obj = null; // 可复活
+        System.gc();
+        Thread.sleep(1000);
+        if(obj == null) {
+            System.out.println("obj is null");
+        } else {
+            System.out.println("obj is live");
+        }
+
+        System.out.println("第二次调用GC");
+        obj = null; //不可复活
+        System.gc();
+        Thread.sleep(1000);
+        if(obj == null) {
+            System.out.println("obj is null");
+        } else {
+            System.out.println("obj is live");
+        }
+    }
+
     @Override
     public void finalize() throws Throwable {
         super.finalize();
--- AksoParenet/AksoDemo/src/main/java/com/akso/jvm/GcTest.java
+++ AksoParenet/AksoDemo/src/main/java/com/akso/jvm/GcTest.java
@@ -1,32 +1,8 @@
 package com.akso.jvm;
 
 public class GcTest {
-    public static void main(String[] args) throws InterruptedException {
-        CanReliveObj obj = new CanReliveObj();
-        obj = null; // 可复活
-
-        System.gc();
-
-        Thread.sleep(1000);
-        if(obj == null) {
-            System.out.println("obj is null");
-        } else {
-            System.out.println("obj is live");
-        }
-
-        System.out.println("第二次调用GC");
-        obj = null; //不可复活
-        System.gc();
-
-        Thread.sleep(1000);
-        if(obj == null) {
-            System.out.println("obj is null");
-        } else {
-            System.out.println("obj is live");
-        }
 
 
-    }
 
 
 }
