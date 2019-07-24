d830ae7f265f854bcefd1eda02a191670a25770e
leo365zhou <leo_365@126.com>
2019-07-24 18:33:31 +0800

    init

--- AksoParenet/AksoDemo/src/main/java/com/akso/algorithm/sort/MergeSort.java
+++ AksoParenet/AksoDemo/src/main/java/com/akso/algorithm/sort/MergeSort.java
@@ -9,13 +9,36 @@ public class MergeSort {
 
     @Test
     public void test1() {
-        mergeSort(arr);
-        print(arr);
+        int[] result = mergeSort(arr);
+        print(result);
     }
 
-    public void mergeSort(int[] arr) {
+    public int[] mergeSort(int[] arr) {
+        if(arr.length < 2) {
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+        return merge(mergeSort(left), mergeSort(right));
+    }
 
-
+    public int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int lt = 0;
+        int r = 0;
+        for(int i=0; i < result.length; i++) {
+            if(lt >= left.length) {
+                result[i] = right[r++];
+            } else if(r >= right.length) {
+                result[i] = left[lt++];
+            } else if(left[lt] <= right[r] )  {
+                result[i] = left[lt++];
+            } else {
+                result[i] = right[r++];
+            }
+        }
+        return result;
     }
 
     public void swap(int[] arr, int i, int j) {
