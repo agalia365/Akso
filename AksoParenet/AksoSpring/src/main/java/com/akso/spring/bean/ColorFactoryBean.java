ae1d6c8a1d0ce026d15b64a812f04b57dc2a1269
git365zhou <leo_365@126.com>
2019-07-29 23:30:29 +0800

    init

---
+++ AksoParenet/AksoSpring/src/main/java/com/akso/spring/bean/ColorFactoryBean.java
@@ -0,0 +1,35 @@
+package com.akso.spring.bean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * 创建一个spring 定义的工厂bean
+ */
+public class ColorFactoryBean implements FactoryBean<Color> {
+    /**
+     *  返回一个Color 对象
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Color getObject() throws Exception {
+        System.out.println("getObject");
+        return new Color();
+    }
+
+    /**
+     * 控制是否是单例
+     * true：这个bean 是单实例， 在容器中保存一份
+     * false： 这是一个多实例， 每次获取都会创建一个实例
+     * @return
+     */
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Color.class;
+    }
+}
--- AksoParenet/AksoSpring/src/test/java/CommonConfigTest.java
+++ AksoParenet/AksoSpring/src/test/java/CommonConfigTest.java
@@ -1,3 +1,5 @@
+import com.akso.spring.bean.Color;
+import com.akso.spring.bean.ColorFactoryBean;
 import com.akso.spring.bean.Person;
 import com.akso.spring.config.CommonConfig;
 import com.akso.spring.config.CommonConfig2;
@@ -35,6 +37,21 @@ public class CommonConfigTest {
     @Test
     public void testImport() {
         AnnotationConfigApplicationContext context =  new AnnotationConfigApplicationContext(CommonConfig2.class);
+        printBeans(context);
+    }
+
+    @Test
+    public void testFactoryBean() {
+        AnnotationConfigApplicationContext context =  new AnnotationConfigApplicationContext(CommonConfig2.class);
+        Color bean1 = (Color) context.getBean("colorFactoryBean");
+        Color bean2 = (Color) context.getBean("colorFactoryBean");
+        System.out.println(bean1 == bean2);
+
+        /**
+         * 要获取工厂bean 本身， 我们需要给bean id 加一个前缀 & ， 这样就可以获取到工厂bean 本身
+         */
+        Object bean3 =  context.getBean("&colorFactoryBean");
+        System.out.println(bean3.getClass());
         printBeans(context);
     }
 
