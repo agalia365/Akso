710a9ffe82abe3587e1faa338d04a63900fe3562
git365zhou <leo_365@126.com>
2019-08-06 00:03:26 +0800

    init

---
+++ demo/src/main/java/com/akso/springcloud/demo/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.akso.springcloud.demo.dao;
+
+import com.akso.springcloud.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
