3aa7ead3d33dea8c1b710e9a5d61b972dc7b9189
leo365zhou <leo_365@126.com>
2019-08-06 17:39:25 +0800

    init

--- microservice-spring-cloud-parent/microservice-discovery-eureka/src/main/java/com/akso/springcloud/erueka/EurekaApplication.java
+++ microservice-spring-cloud-parent/microservice-discovery-eureka/src/main/java/com/akso/springcloud/erueka/EurekaApplication.java
@@ -2,7 +2,6 @@ package com.akso.springcloud.erueka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
