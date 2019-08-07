02ca900deb613960f4cd9ff5798b3d95864bf6f8
leo365zhou <leo_365@126.com>
2019-08-07 14:54:43 +0800

    init

--- microservice-spring-cloud-parent/microservice-provider-user/src/main/java/com/akso/springcloud/demo/MicroserviceProviderUserApplication.java
+++ microservice-spring-cloud-parent/microservice-provider-user/src/main/java/com/akso/springcloud/demo/MicroserviceProviderUserApplication.java
@@ -2,10 +2,10 @@ package com.akso.springcloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MicroserviceProviderUserApplication {
 
     public static void main(String[] args) {
