20189c7d3fdbad253eb8ee7b0870695b6b83ab0c
leo365zhou <leo_365@126.com>
2019-08-08 16:54:42 +0800

    init

--- microservice-spring-cloud-parent/microservice-provider-user/src/main/java/com/akso/springcloud/demo/controller/UserController.java
+++ microservice-spring-cloud-parent/microservice-provider-user/src/main/java/com/akso/springcloud/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.akso.springcloud.demo.controller;
 
 import com.akso.springcloud.demo.entity.User;
 import com.akso.springcloud.demo.service.UserService;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EurekaClient discoveryClient;
+
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable Long id) {
         return userService.findById(id);
     }
 
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = discoveryClient.getNextServerFromEureka("localhost", true);
+        return instance.getHomePageUrl();
+    }
 }
