2e3d3dc4b76051fa15c9672fc48cc849ff919c46
git365zhou <leo_365@126.com>
2019-08-08 23:45:57 +0800

    spring cloud ribbon load balance

---
+++ microservice-spring-cloud-parent/microservice-movie-ribbon/src/main/java/com/akso/spring/cloud/ribbon/MicroserviceMovieRibbonApplication.java
@@ -0,0 +1,25 @@
+package com.akso.spring.cloud.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceMovieRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMovieRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Do any additional configuration here
+        return builder.build();
+    }
+}
