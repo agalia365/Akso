2cd125096671743b77228bbefb7eb6a6b70bc8a9
git365zhou <leo_365@126.com>
2019-08-11 22:33:34 +0800

    feigin wiout hystrix

---
+++ microservice-spring-cloud-parent/microservice-consumer-movie-feign-without-histrix/src/main/java/com/example/microservice/movie/feign/UserFeignClient.java
@@ -0,0 +1,17 @@
+package com.example.microservice.movie.feign;
+
+import com.example.microservice.movie.feign.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "microservice-provider-user",  fallback = FeignClientFallback.class)
+public interface UserFeignClient {
+    @RequestMapping(value = "/simple/{id}", method = RequestMethod.GET)
+    public User findById(@PathVariable("id") Long id );
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User postUser(User user);
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User getUser(User user);
+}
