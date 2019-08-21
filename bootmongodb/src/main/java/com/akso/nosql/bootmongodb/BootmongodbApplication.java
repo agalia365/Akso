2c7b618267036a05af1492ac1d0b5bb9d56e6ef3
git365zhou <leo_365@126.com>
2019-08-21 17:42:21 +0800

    zuul

---
+++ bootmongodb/src/main/java/com/akso/nosql/bootmongodb/BootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.akso.nosql.bootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootmongodbApplication.class, args);
+    }
+
+}
