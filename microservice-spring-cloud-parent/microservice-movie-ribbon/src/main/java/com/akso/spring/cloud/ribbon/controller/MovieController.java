6fc6152ca50a9272388b5d0d3ed2da4c82702c3c
git365zhou <leo_365@126.com>
2019-08-21 19:22:33 +0800

    zuul

--- microservice-spring-cloud-parent/microservice-movie-ribbon/src/main/java/com/akso/spring/cloud/ribbon/controller/MovieController.java
+++ microservice-spring-cloud-parent/microservice-movie-ribbon/src/main/java/com/akso/spring/cloud/ribbon/controller/MovieController.java
@@ -4,7 +4,6 @@ import com.akso.spring.cloud.ribbon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
